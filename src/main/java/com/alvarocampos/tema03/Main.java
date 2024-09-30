485bd8f9fceb149a65428eca2218b4b2840ea36a
Alvaro Campos <alvaro.campos.bolufer.a@gmail.com>
2024-09-30 20:01:37 +0200

    Añadidos los archivos del proyecto

---
+++ src/main/java/com/alvarocampos/tema03/Main.java
@@ -0,0 +1,11 @@
+package com.alvarocampos.tema03;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i = 3;
+        int x = i++;
+        int y = ++i;
+
+    }
+}
